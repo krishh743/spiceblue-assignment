289f10ecf1dbb89f29209c88232246d8531a7ec4
krishna kumar kushwaha <krishnakushwaha743@gmail.com>
2021-07-08 12:24:37 +0530

    first c

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//bootstrap css
+
+function App(){
+
+
+return (
+  <div className="App">
+<h1>Task maker</h1>
+<Header/>
+
+  
+  </div>
+);
+}
+export default App;
+     
